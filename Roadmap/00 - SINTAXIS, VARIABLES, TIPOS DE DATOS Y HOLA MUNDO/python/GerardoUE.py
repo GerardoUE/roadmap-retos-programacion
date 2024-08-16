e5a7b7d57d15248ce652443cd7273f780485b0d0
GerardoUE <gerardue@gmail.com>
2024-08-16 15:38:16 -0500

    add GerardoUE 00 - python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/GerardoUE.py
@@ -0,0 +1,25 @@
+#https://www.python.org/
+
+#Para comentar una sola línea se utiliza el símbolo numeral "#"
+
+"""
+Para comentar varias líneas se utiliza triple comilla doble 
+y de esa manera 
+nos permite comentar 
+en varias
+lineas
+"""
+
+#variable y constante
+saludo = "Hola Moure" #variable normal que se puede modificar
+ESTADO_ANIMO = "Como te encuentras hoy ?" #constante que no se puede modificar , se crea en un arthivo aparte y se realiza el llamado.
+
+#Datos primitivos
+texto = "Este es una cadna de texto" #String
+numeroEntero = 10 #Numero entero / Int
+numeroFlotante = 10.5 #Numero flotante / Float
+verdadero = True #Booleano / Bool
+falso = False #Booleano / Bool
+
+print('¡Hola Python!')
+
